f2a0f513478e8f0cf3aa2688ff3456bc336f7450
Alexandra Rodriguez-Beuerman <alexandra@classkick.com>
2022-08-31 09:52:31 -0400

    add api calls

---
+++ src/api/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const url = 'https://crystal-pepsi.herokuapp.com/students';
+const token =  '2444011b-7b70-4608-aa17-bdfb67359553';
+
+const config = {
+    headers: { Authorization: `${token}` }
+};
+
+export async function getStudents(){
+  const response = await axios.get(url,config)
+
+  // axios.get(url, config)
+  console.log('in api call', response.data);
+  return response.data;
+}
+
+export async function getStudentMeetings(studentName){
+  const studentUrl = `${url}/${studentName}/meetings`;
+  const response = await axios.get(studentUrl, config);
+  console.log(response);
+}
